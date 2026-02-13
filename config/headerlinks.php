60a7daef02860863ed677a4321ad3e0af7ad2c88
gabrieletodaroo9 <gabrieletodaroo9@gmail.com>
2026-02-13 02:18:46 +0100

    create header and config the navbar into the config folder

---
+++ config/headerlinks.php
@@ -0,0 +1,45 @@
+<?php
+
+return [
+[
+"name" => "CHARACTER",
+"path" => "/character"
+],
+[
+"name" => "COMICS",
+"path" => "/comics"
+],
+[
+"name" => "MOVIES",
+"path" => "/movies"
+],
+[
+"name" => "TV" ,
+"path" => "/tv"
+],
+[
+"name" => "GAMES" ,
+"path" => "/games"
+],
+[
+"name" => "COLLECTIBLES" ,
+"path" => "/collectibles"
+],
+[
+"name" => "VIDEOS" ,
+"path" => "/videos"
+],
+[
+"name" => "FANS" ,
+"path" => "/fans"
+],
+[
+"name" => "NEWS" ,
+"path" => "/news"
+],
+[
+"name" => "SHOP" ,
+"path" => "/shop"
+],
+];
+?>
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,3 +1,18 @@
+
 <header>
- sono l'header
+    <nav class="container d-flex justify-content-center justify-content-md-between align-items-center">
+        <a href="#" class="logo">
+            {{-- Assicurati che $src sia definita o usa un percorso statico --}}
+            <img src="{{ Vite::asset('assets/img/dc-logo.png') }}" alt="logo-dc" class="w-50">
+        </a>
+        <ul class="list-unstyled d-flex flex-wrap gap-5">
+
+            @foreach(config('headerlinks') as $link)
+                <li class="nav-link py-4">
+                    <a href="{{ $link['path'] }}">{{ $link['name'] }}</a>
+                </li>
+            @endforeach
+
+        </ul>
+    </nav>
 </header>
